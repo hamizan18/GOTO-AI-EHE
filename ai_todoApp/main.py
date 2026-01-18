a43bf96a1062e29e47cfb75bfde59d2b37c6934d
Mijaenn <hamizanputra321@gmail.com>
2026-01-18 20:03:28 +0700

    mulai AI learn - serius

---
+++ ai_todoApp/main.py
@@ -0,0 +1,8 @@
+# ambil input
+task = input("Input your Task: ")
+deadline = input("Input deadline date: ")
+important = input("Input the important tier (use number): ")
+
+# logika AI
+
+# tampilkan hasil
