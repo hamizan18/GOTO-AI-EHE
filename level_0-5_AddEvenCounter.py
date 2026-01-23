ef7650314efb595bef3f7b377c9804e3ee80fdc8
Mijaenn <hamizanputra321@gmail.com>
2026-01-23 20:02:11 +0700

    show time hajimari level 5

--- level_0-5_AddEvenCounter.py
+++ level_0-5_AddEvenCounter.py
@@ -1,3 +1,16 @@
+namba = []
+
 def Counter():
-    namba = []
-    numbs = input('Input Your Number here: ')
+
+    while True:
+        numbs = input('Input Your Number (fill blank or type "x" to close): ')
+        if numbs != 'x' and numbs != '':
+            namba.append(numbs)
+        if numbs == 'x' or numbs == '':
+            break
+        
+    print("All your number is: ", end="")
+    for i in range(0, len(namba)):
+        print(namba[i], end=" ")
+
+Counter()
