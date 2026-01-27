73487be74e03bb75b97f93387b8221cb07de8dfe
Mijaenn <hamizanputra321@gmail.com>
2026-01-27 20:08:17 +0700

    init level 1.1

---
+++ level_1-1_tenFunction.py
@@ -0,0 +1,28 @@
+def summation():
+    
+    
+def minMax():
+    
+    
+def checkOdd():
+    
+    
+def checkMax():
+    
+    
+def averageNumber():
+    
+    
+def primeCheck():
+    
+    
+def factorial():
+
+
+def subtraction():
+
+
+def divine():
+
+
+def multiply():
