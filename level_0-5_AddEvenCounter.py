a7b3bfa713f84ace15e2633743e8e195ed472172
Mijaenn <hamizanputra321@gmail.com>
2026-01-27 19:37:20 +0700

    level 0.5 clear

--- level_0-5_AddEvenCounter.py
+++ level_0-5_AddEvenCounter.py
@@ -4,12 +4,13 @@ def Counter():
     even = 0
 
     while True:
-        numbs = int(input('Input Your Number (fill blank to close): '))
+        numbs = input('Input Your Number (fill blank to close): ')
         if numbs != '':
+            numbs = int(numbs)
             if numbs % 2 == 0:
-                odd += 1
-            else:
                 even += 1
+            else:
+                odd += 1
             namba.append(numbs)
         if numbs == '':
             break
@@ -19,8 +20,7 @@ def Counter():
     for i in range(0, len(namba)):
         print(namba[i], end=" ")
 
-    print("Total count of Odd number is: " + odd)
-    print("Total count of Even number is: " + even)
+    print(f"\nTotal count of Odd number is: {odd}")
+    print(f"Total count of Even number is: {even} ")
 
 Counter()
-# AA PLISSS LIAT NI PENGEN LANJUUTS
