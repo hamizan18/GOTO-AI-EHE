95ff0d383d8e2567331894721b61f410da40f990
Mijaenn <hamizanputra321@gmail.com>
2026-01-28 21:23:13 +0700

    harini ada interview ringan dari narasumber tentang codingan

--- level_0-1_largestNumber.py
+++ level_0-1_largestNumber.py
@@ -1,8 +1,9 @@
 print("Enter a number:")
-number1 = int(input(f'Enter number 1 : '))
+number1 = int(input('Enter number 1 : '))
 for i in range(2, 6):
     number = int(input(f'Enter number {i} : '))
     if number > number1:
         number1 = number
 
 print(f'\nThe largest number from your 5 input is: {number1}')
+
---
+++ said babi.py
@@ -0,0 +1,9 @@
+text = input('Apakah said adalah anak pak said? ')
+
+
+if text == 'ya':
+    print("prabowo cabul")
+elif text == 'ga':
+    print("said tidak cabul")
+else:
+    print("said cabul banget")
