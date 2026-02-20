76f72013891614d9e64c4afce7dd6a5026c3c35d
Mijaenn <hamizanputra321@gmail.com>
2026-02-20 11:00:16 +0700

    sorting hajime

---
+++ level_1-1.1_sort.py
@@ -0,0 +1,17 @@
+array_awal = [8, 3, 1, 6, 2, 9, 7, 4, 5]
+
+def bubbelSort():
+    print('Array awal sebelum bubble sort: ')
+    for number in array_awal:  
+        print(number, end=" ")
+    
+    
+    
+def selectionSort():
+    print('Array awal sebelum selection sort: ')
+    for number in array_awal:
+        print(number, end=" ")
+        
+bubbelSort()
+print('\n')
+selectionSort()
