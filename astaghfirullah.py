31775238717480b91d1a15687319345c6b400e78
hamizan18 <hamizanputra321@gmail.com>
2026-03-24 15:29:44 +0700

    prak1 tija

--- astaghfirullah.py
+++
@@ -1,9 +0,0 @@
-text = input('Apakah said adalah anak pak said? ')
-
-
-if text == 'ya':
-    print("prabowo cabul")
-elif text == 'ga':
-    print("said tidak cabul")
-else:
-    print("said cabul banget")
