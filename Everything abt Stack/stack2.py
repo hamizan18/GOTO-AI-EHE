4ad805a6cff9952347a38fd04d6734f78dda5052
hamizan18 <hamizanputra321@gmail.com>
2026-03-25 17:55:06 +0700

    lvl 2 memasuki stack done

---
+++ Everything abt Stack/stack2.py
@@ -0,0 +1,9 @@
+for char in "([])":
+    if char == "(":
+        print("Buka kurung")
+    elif char == ")":
+        print("tutup kurung")
+    elif char == "[":
+        print("[", end="")
+    elif char == "]":
+        print("]")
