7a221af87f3cb5de4bbaa2370e1c6d4f086f92e2
hamizan18 <hamizanputra321@gmail.com>
2026-04-08 18:41:43 +0700

    big data play

---
+++ BIG DATA/numpyL.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+x = [0, 1, 2, 3, 4]
+
+print(x)
---
+++ BIG DATA/numpyL2.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy import random
+
+t = np.empty(5)
+
+# for i in range(5):
+#     t[i] = i * 2
+
+# for number in t:
+#     print(number, end=" ")
+    
+# print("\n")
+
+
+# # versi clean n pro nya
+# for i, val in enumerate(t):
+#     print(val, end=" ")
+
+result = []
+for x in [1, 2, 3, 4]:
+    result.append(x * 3)
+
+print(x)
